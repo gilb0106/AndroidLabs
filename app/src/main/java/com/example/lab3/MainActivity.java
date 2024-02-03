861c508868de7821f416aac12090dd5fc3c3301f
gilb0106 <gilb0106@algonquinlive.com>
2024-02-03 18:48:58 -0400

    roughly all requirements met, fine tuning still required

--- app/src/main/java/com/example/lab3/MainActivity.java
+++ app/src/main/java/com/example/lab3/MainActivity.java
@@ -1,14 +1,15 @@
 package com.example.lab3;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
-
+import com.example.lab3.NameActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final int BUTTON_RESULT = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,14 +18,24 @@ public class MainActivity extends AppCompatActivity {
         EditText editText = findViewById(R.id.editText);
         Button nextButton = findViewById(R.id.button);
 
+
         Intent nextPage = new Intent(MainActivity.this, NameActivity.class);
         nextButton.setOnClickListener(click -> {
             nextPage.putExtra("name", editText.getText().toString());
 
-        startActivity( nextPage );  });
 
+            startActivityForResult(nextPage, BUTTON_RESULT);  /*
+            For result to expect return to either close or stay on this page */
 
-
-
+        });
     }
-}
+        @Override
+        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+            super.onActivityResult(requestCode, resultCode, data);
+            if (requestCode == BUTTON_RESULT) {
+                if (resultCode == 0) {// if 0 do nothing, just stay on page
+                } else if (resultCode == 1)  // shut er down
+                    finish();
+                }
+            }
+        }
--- app/src/main/java/com/example/lab3/NameActivity.java
+++ app/src/main/java/com/example/lab3/NameActivity.java
@@ -1,9 +1,11 @@
 package com.example.lab3;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class NameActivity extends AppCompatActivity {
@@ -13,8 +15,20 @@ public class NameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_name);
         Intent dataSent = getIntent();
-        String nameSent = dataSent.getStringExtra("name");
+        String inputText = dataSent.getStringExtra("name");
         TextView textView = findViewById(R.id.textView);
-        textView.setText(nameSent);
+        textView.setText(inputText);
+        Button DCMT = findViewById(R.id.DCMT);
+        Button TY = findViewById(R.id.TY);
+
+        DCMT.setOnClickListener(click -> {
+            setResult(0);//dont call me that, previous page
+            finish();
+        });
+        
+        TY.setOnClickListener(click -> {
+            setResult(1); //Thank you button, close app
+            finish();
+        });
     }
 }
